3394a25342345cfa9b9f36f8854342e2882510c3
Moussa Diatta <diattamoussa010@gmail.com>
2023-10-29 18:00:17 +0000

    Partie details des pays

--- script.js
+++ script.js
@@ -31,6 +31,9 @@ function showCountry(data){
     <p><strong>Capital :</strong>${data.capital}</p>
 </div>`
     countriesElem.appendChild(country)
+    country.addEventListener("click", ()=>{
+        showCountryDetail(data)
+    })
 }
 
 dropDown.addEventListener("click", () =>{
@@ -67,3 +70,39 @@ toggle.addEventListener("click", ()=>{
     document.body.classList.toggle("dark")
     lune.classList.toggle("fa-moon")
 })
+
+//-Affichage des details ainsi le bouton 'Retour'
+
+const countryModal = document.querySelector(".countryModal")
+
+function showCountryDetail(data){
+    countryModal.classList.toggle("show")
+    countryModal.innerHTML = `<button class="back">Back</button>
+    <div class="modal">
+        <div class="leftModal">
+            <img src="${data.flags.png}" alt="">
+        </div>
+        <div class="rightModal">
+            <h1>${data.name.common}</h1>
+            <div class="modalInfo">
+                <div class="innerLeft inner">
+                    <p><strong>Native Name :</strong>${data.name.official}</p>
+                    <p><strong>Population :</strong>${data.population}</p>
+                    <p><strong>Region :</strong>${data.region}</p>
+                    <p><strong>Sub-region :</strong>${data.subregion}</p>
+                </div>
+                <div class="innerRight inner">
+                    <p><strong>Capital :</strong>${data.capital}</p>
+                    <p><strong>Top Level Domain :</strong>${data.tld}</p>
+                    <p><strong>Currencies :</strong>${data.currencies}</p>
+                    <p><strong>Languages :</strong>${data.languages}</p>
+                </div>
+            </div>
+            <p class="border"><strong>Borders :</strong>${data.borders}</p>
+        </div>
+    </div>`
+    const back = countryModal.querySelector(".back")
+    back.addEventListener("click", ()=>{
+        countryModal.classList.toggle("show")
+    });
+}
